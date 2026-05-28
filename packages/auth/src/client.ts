c1c9b2463f42ab80fdaddb44061b3f1dfe831335
Ryan Luong <lumintriet@gmail.com>
2026-05-28 09:27:46 +0700

    build(auth/build): build package.

--- packages/auth/src/client.ts
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-import { authClientEnvVars } from "@/env-vars/client";
-
-export const authClient = createAuthClient({
-  baseURL: authClientEnvVars.VITE_BETTER_AUTH_URL,
-});
