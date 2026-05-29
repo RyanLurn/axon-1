994ae7b2a604fed602aa2b5c43717994c269846f
Ryan Luong <lumintriet@gmail.com>
2026-05-29 19:36:47 +0700

    feat(auth/user): create userAuth instance.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,9 +4,12 @@ import { betterAuth } from "better-auth";
 import { emailPasswordOptions } from "@/options/email-password";
 import { baseAuthOptions } from "@/options/base";
 
-export const auth = betterAuth({
+export const userAuth = betterAuth({
   ...baseAuthOptions,
-  emailAndPassword: emailPasswordOptions,
+  emailAndPassword: {
+    ...emailPasswordOptions,
+    disableSignUp: true,
+  },
   // Better Auth docs specifies that the tanstackStartCookies plugin must come last in the array.
   plugins: [tanstackStartCookies()],
 });
--- packages/auth/src/options/email-password.ts
+++ packages/auth/src/options/email-password.ts
@@ -2,8 +2,8 @@ import type { BetterAuthOptions } from "better-auth";
 
 import { MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH } from "@/constants";
 
-export const emailPasswordOptions: BetterAuthOptions["emailAndPassword"] = {
+export const emailPasswordOptions = {
   enabled: true,
   minPasswordLength: MIN_PASSWORD_LENGTH,
   maxPasswordLength: MAX_PASSWORD_LENGTH,
-};
+} satisfies BetterAuthOptions["emailAndPassword"];
