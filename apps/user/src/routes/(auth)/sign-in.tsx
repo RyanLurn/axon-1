e69cd3d622ecd4fc99359d952a98992e738afda5
Ryan Luong <lumintriet@gmail.com>
2026-05-29 09:29:58 +0700

    feat(user/auth): give feedback on error.

--- apps/user/src/routes/(auth)/sign-in.tsx
+++ apps/user/src/routes/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useForm } from "@tanstack/react-form";
+import { toast } from "sonner";
 
 import { Route as ChatRoute } from "@/routes/_authenticated/chat";
 import { credentialsValidator } from "@/lib/auth/validators";
@@ -12,11 +13,15 @@ export const Route = createFileRoute("/(auth)/sign-in")({
 function SignInPage() {
   const signInForm = useForm({
     onSubmit: async ({ value }) => {
-      const { data, error } = await authClient.signIn.email({
+      const { error } = await authClient.signIn.email({
         ...value,
         rememberMe: true,
         callbackURL: ChatRoute.to,
       });
+
+      if (error) {
+        toast.error(error.message ?? "Failed to sign in.");
+      }
     },
     defaultValues: {
       email: "",
