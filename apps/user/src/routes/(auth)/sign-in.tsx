2a58f420b19ff52a2a254af9a2c380c4deca54af
Ryan Luong <lumintriet@gmail.com>
2026-05-29 08:59:00 +0700

    feat(user/auth): create signInForm from useForm hook.

--- apps/user/src/routes/(auth)/sign-in.tsx
+++ apps/user/src/routes/(auth)/sign-in.tsx
@@ -1,9 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useForm } from "@tanstack/react-form";
+
+import { Route as ChatRoute } from "@/routes/_authenticated/chat";
+import { authClient } from "@/lib/auth/client";
 
 export const Route = createFileRoute("/(auth)/sign-in")({
   component: SignInPage,
 });
 
 function SignInPage() {
+  const signInForm = useForm({
+    onSubmit: async ({ value }) => {
+      const { data, error } = await authClient.signIn.email({
+        ...value,
+        rememberMe: true,
+        callbackURL: ChatRoute.to,
+      });
+    },
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return <div>Hello "/(auth)/sign-in"!</div>;
 }
