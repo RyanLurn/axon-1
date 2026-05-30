3d1c966ec3bd496105fc23039ea5d8a1243c549a
Ryan Luong <lumintriet@gmail.com>
2026-05-30 16:11:26 +0700

    feat(fs/types): define RealPath type.

--- packages/fs/src/types.ts
+++ packages/fs/src/types.ts
@@ -1,5 +1,7 @@
 import type { Branded } from "@repo/types/branded";
 
-export type DirectoryPath = Branded<string, "DirectoryPath">;
+export type RealPath = Branded<string, "FilePath">;
 
 export type FilePath = Branded<string, "FilePath">;
+
+export type DirectoryPath = Branded<string, "DirectoryPath">;
