fbdc879e5a0d846d7790b3f7ce52cbd265ab1452
Ryan Luong <lumintriet@gmail.com>
2026-05-30 08:56:13 +0700

    feat(git/commands): pipe stderr in spawnGitReceivePackAdvertisement function.

--- packages/git/src/commands/receive-pack/advertisement.ts
+++ packages/git/src/commands/receive-pack/advertisement.ts
@@ -3,13 +3,21 @@ export async function spawnGitReceivePackAdvertisement({
 }: {
   repoPath: string;
 }) {
-  const gitProcess = Bun.spawn([
-    "git",
-    "receive-pack",
-    "--http-backend-info-refs",
-    repoPath,
-  ]);
+  const gitProcess = Bun.spawn(
+    ["git", "receive-pack", "--http-backend-info-refs", repoPath],
+    {
+      stdout: "pipe",
+      stderr: "pipe",
+    }
+  );
 
+  const exitCode = await gitProcess.exited;
   const output = await gitProcess.stdout.text();
-  return output;
+  const error = await gitProcess.stderr.text();
+
+  return {
+    exitCode,
+    output,
+    error,
+  };
 }
