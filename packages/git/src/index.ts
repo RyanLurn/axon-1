40f514162cb9ff101061c4f3b92b7747de48b2e9
Ryan Luong <lumintriet@gmail.com>
2026-05-31 09:33:13 +0700

    feat(git/upload-pack): add spawnUploadPackAdvertisement function to /:repoName/info/refs handler.

--- packages/git/src/index.ts
+++ packages/git/src/index.ts
@@ -1,8 +1,11 @@
 import { validator } from "hono/validator";
 import { prettifyError } from "zod";
+import { join } from "node:path";
 import { Hono } from "hono";
 
+import { spawnUploadPackAdvertisement } from "@/services/upload-pack/advertisement";
 import { gitServiceValidator, repoNameValidator } from "@/validators";
+import { gitEnvVars } from "@/env-vars";
 
 export const gitServer = new Hono().get(
   "/:repoName/info/refs",
@@ -40,7 +43,52 @@ export const gitServer = new Hono().get(
 
     return queryValidationResult.data;
   }),
-  (c) => {
-    return c.json({ data: "Yay!" }, 200);
+  async (c) => {
+    const repoName = c.req.valid("param");
+    const service = c.req.valid("query");
+
+    const repoPath = join(gitEnvVars.GIT_DIR_PATH, repoName);
+
+    if (service === "git-upload-pack") {
+      const spawnResult = await spawnUploadPackAdvertisement(repoPath);
+
+      if (!spawnResult.isOk) {
+        const error = spawnResult.error;
+        console.error(error);
+
+        if (error.code === "NO_ENTRY_ERROR") {
+          return c.json(
+            {
+              error: {
+                code: "NOT_FOUND_ERROR",
+                message: `The repo named ${repoName} doesn't exist.`,
+              },
+            },
+            404
+          );
+        }
+
+        return c.json(
+          {
+            error: { code: "INTERNAL_ERROR", message: "Something went wrong." },
+          },
+          500
+        );
+      }
+
+      c.header("Content-Type", `application/x-${service}-advertisement`);
+      c.header("Cache-Control", "no-cache");
+      return c.text(spawnResult.data, 200);
+    }
+
+    return c.json(
+      {
+        error: {
+          code: "UNSUPPORTED_SERVICE_ERROR",
+          message: `The ${service} service is currently unsupported.`,
+        },
+      },
+      400
+    );
   }
 );
