17fb83a36084a3519c1792df7d469f60fbd5e59e
Ryan Luong <lumintriet@gmail.com>
2026-05-31 08:46:04 +0700

    feat(git/validators): improve repoNameValidator.

--- packages/git/src/constants.ts
+++ packages/git/src/constants.ts
@@ -1,4 +1,3 @@
-export const REPO_NAME_MIN_LENGTH = 1;
 export const REPO_NAME_MAX_LENGTH = 100;
 export const REPO_NAME_ALLOWED_CHARACTERS_REGEX = /^[a-zA-Z0-9_-]+$/;
 
--- packages/git/src/validators.ts
+++ packages/git/src/validators.ts
@@ -3,14 +3,15 @@ import { z } from "zod";
 import {
   REPO_NAME_ALLOWED_CHARACTERS_REGEX,
   REPO_NAME_MAX_LENGTH,
-  REPO_NAME_MIN_LENGTH,
   GIT_SERVICES,
 } from "@/constants";
 
 export const repoNameValidator = z
   .string()
-  .min(REPO_NAME_MIN_LENGTH)
   .max(REPO_NAME_MAX_LENGTH)
-  .regex(REPO_NAME_ALLOWED_CHARACTERS_REGEX);
+  .regex(REPO_NAME_ALLOWED_CHARACTERS_REGEX)
+  .refine((name) => !name.startsWith("-") && !name.endsWith("-"), {
+    error: "Repo name cannot start or end with a hyphen",
+  });
 
 export const gitServiceValidator = z.enum(GIT_SERVICES);
