e7b3c866c6fddcf99b24e4e461f1596cb5b93412
Ryan Luong <lumintriet@gmail.com>
2026-05-31 08:06:42 +0700

    feat(git/validators): create repoNameValidator.

---
+++ packages/git/src/validators.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import {
+  REPO_NAME_ALLOWED_CHARACTERS_REGEX,
+  REPO_NAME_MAX_LENGTH,
+  REPO_NAME_MIN_LENGTH,
+} from "@/constants";
+
+export const repoNameValidator = z
+  .string()
+  .min(REPO_NAME_MIN_LENGTH)
+  .max(REPO_NAME_MAX_LENGTH)
+  .regex(REPO_NAME_ALLOWED_CHARACTERS_REGEX);
