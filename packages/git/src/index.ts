cca9ed9d1887b33193d049926badfcc1f41298da
Ryan Luong <lumintriet@gmail.com>
2026-05-31 08:20:57 +0700

    feat(git/advertise): add path param validation to /info/refs route.

--- packages/git/src/index.ts
+++ packages/git/src/index.ts
@@ -1,3 +1,27 @@
+import { validator } from "hono/validator";
+import { prettifyError } from "zod";
 import { Hono } from "hono";
 
-export const gitServer = new Hono().get("/:repo/info/refs");
+import { repoNameValidator } from "@/validators";
+
+export const gitServer = new Hono().get(
+  "/:repoName/info/refs",
+  validator("param", (value, c) => {
+    const validationResult = repoNameValidator.safeParse(value["repoName"]);
+
+    if (!validationResult.success) {
+      console.log(prettifyError(validationResult.error));
+      return c.json(
+        {
+          error: { code: "VALIDATION_ERROR", message: "Invalid repo name." },
+        },
+        400
+      );
+    }
+
+    return validationResult.data;
+  }),
+  (c) => {
+    return c.json({ data: "Yay!" }, 200);
+  }
+);
