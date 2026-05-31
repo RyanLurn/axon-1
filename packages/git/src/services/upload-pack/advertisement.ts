84bbe994af4713172be707f3747717d838b0b05d
Ryan Luong <lumintriet@gmail.com>
2026-05-31 09:17:34 +0700

    feat(git/upload-pack): add error handling and Result type to spawnUploadPackAdvertisement function.

--- packages/git/src/services/upload-pack/advertisement.ts
+++ packages/git/src/services/upload-pack/advertisement.ts
@@ -1,19 +1,52 @@
-export async function spawnUploadPackAdvertisement(repoPath: string) {
-  const gitProcess = Bun.spawn(
-    ["git", "upload-pack", "--http-backend-info-refs", repoPath],
-    {
-      stdout: "pipe",
-      stderr: "pipe",
+import type { NoEntryError } from "@repo/fs/errors/no-entry";
+import type { Result } from "@repo/types/result";
+
+import { resolveRealPath } from "@repo/fs/resolve-real-path";
+import { UnexpectedError } from "@repo/errors/unexpected";
+
+export async function spawnUploadPackAdvertisement(
+  repoPath: string
+): Promise<Result<string, UnexpectedError | NoEntryError>> {
+  try {
+    const gitProcess = Bun.spawn(
+      ["git", "upload-pack", "--http-backend-info-refs", repoPath],
+      {
+        stdout: "pipe",
+        stderr: "pipe",
+      }
+    );
+
+    const exitCode = await gitProcess.exited;
+
+    if (exitCode !== 0) {
+      const error = await gitProcess.stderr.text();
+
+      const resolveRealPathResult = await resolveRealPath(repoPath);
+      if (!resolveRealPathResult.isOk) {
+        return resolveRealPathResult;
+      }
+
+      return {
+        isOk: false,
+        error: new UnexpectedError({
+          message: `Something went wrong while spawning upload-pack service's advertisement for repo at ${repoPath}.`,
+          cause: new Error(error),
+        }),
+      };
     }
-  );
 
-  const exitCode = await gitProcess.exited;
-  const output = await gitProcess.stdout.text();
-  const error = await gitProcess.stderr.text();
-
-  return {
-    exitCode,
-    output,
-    error,
-  };
+    const output = await gitProcess.stdout.text();
+    return {
+      isOk: true,
+      data: output,
+    };
+  } catch (error) {
+    return {
+      isOk: false,
+      error: new UnexpectedError({
+        message: `Something went wrong while spawning upload-pack service's advertisement for repo at ${repoPath}.`,
+        cause: error,
+      }),
+    };
+  }
 }
