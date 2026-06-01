9423f0c2bac730e0cc98dfbd7d16a586a63d313c
Ryan Luong <lumintriet@gmail.com>
2026-06-01 08:08:59 +0700

    feat(git/receive-pack): ise spawnReceivePack function in POST /:repo/receive-pack route handler.

--- packages/git/src/index.ts
+++ packages/git/src/index.ts
@@ -4,6 +4,7 @@ import { join } from "node:path";
 import { Hono } from "hono";
 
 import { gitServiceValidator, repoNameValidator } from "@/validators";
+import { spawnReceivePack } from "@/services/receive-pack";
 import { spawnInfoRefsAd } from "@/services/info-refs";
 import { gitEnvVars } from "@/env-vars";
 
@@ -57,6 +58,31 @@ export const gitServer = new Hono()
       return c.text(spawnResult.data, 200);
     }
   )
-  .post("/:repo/receive-pack", repoPathParamValidator, (c) => {
-    return c.text("OK", 200);
+  .post("/:repo/receive-pack", repoPathParamValidator, async (c) => {
+    const repo = c.req.valid("param");
+    const repoPath = join(gitEnvVars.GIT_DIR_PATH, repo);
+
+    const requestBody = c.req.raw.body;
+
+    if (requestBody === null) {
+      return c.text("Invalid request", 400);
+    }
+
+    const spawnResult = await spawnReceivePack({ repoPath, requestBody });
+
+    if (!spawnResult.isOk) {
+      const error = spawnResult.error;
+      console.error(error);
+
+      if (error.code === "NO_ENTRY_ERROR") {
+        return c.text("Repository not found", 404);
+      }
+
+      return c.text("Internal server error", 500);
+    }
+
+    c.header("Content-Type", "application/x-git-receive-pack-result");
+    c.header("Cache-Control", "no-cache");
+    console.log(spawnResult.data);
+    return c.text(spawnResult.data, 200);
   });
