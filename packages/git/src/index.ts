35a68d0a4c7af33b0387b09188b54b375283aec7
Ryan Luong <lumintriet@gmail.com>
2026-06-01 07:42:06 +0700

    feat(git/receive-pack): add route handler.

--- packages/git/src/index.ts
+++ packages/git/src/index.ts
@@ -7,50 +7,54 @@ import { gitServiceValidator, repoNameValidator } from "@/validators";
 import { spawnInfoRefsAd } from "@/services/info-refs";
 import { gitEnvVars } from "@/env-vars";
 
-export const gitServer = new Hono().get(
-  "/:repo/info/refs",
-  validator("param", (value, c) => {
-    const validationResult = repoNameValidator.safeParse(value["repo"]);
+export const gitServer = new Hono()
+  .get(
+    "/:repo/info/refs",
+    validator("param", (value, c) => {
+      const validationResult = repoNameValidator.safeParse(value["repo"]);
 
-    if (!validationResult.success) {
-      console.log(prettifyError(validationResult.error));
-      return c.text("Invalid request", 400);
-    }
-
-    return validationResult.data;
-  }),
-  validator("query", (value, c) => {
-    const validationResult = gitServiceValidator.safeParse(value["service"]);
-
-    if (!validationResult.success) {
-      console.log(prettifyError(validationResult.error));
-      return c.text("Invalid request", 400);
-    }
-
-    return validationResult.data;
-  }),
-  async (c) => {
-    const repo = c.req.valid("param");
-    const service = c.req.valid("query");
-
-    const repoPath = join(gitEnvVars.GIT_DIR_PATH, repo);
-
-    const spawnResult = await spawnInfoRefsAd({ repoPath, service });
-
-    if (!spawnResult.isOk) {
-      const error = spawnResult.error;
-      console.error(error);
-
-      if (error.code === "NO_ENTRY_ERROR") {
-        return c.text("Repository not found", 404);
+      if (!validationResult.success) {
+        console.log(prettifyError(validationResult.error));
+        return c.text("Invalid request", 400);
       }
 
-      return c.text("Internal server error", 500);
-    }
+      return validationResult.data;
+    }),
+    validator("query", (value, c) => {
+      const validationResult = gitServiceValidator.safeParse(value["service"]);
 
-    c.header("Content-Type", `application/x-${service}-advertisement`);
-    c.header("Cache-Control", "no-cache");
-    console.log(spawnResult.data);
-    return c.text(spawnResult.data, 200);
-  }
-);
+      if (!validationResult.success) {
+        console.log(prettifyError(validationResult.error));
+        return c.text("Invalid request", 400);
+      }
+
+      return validationResult.data;
+    }),
+    async (c) => {
+      const repo = c.req.valid("param");
+      const service = c.req.valid("query");
+
+      const repoPath = join(gitEnvVars.GIT_DIR_PATH, repo);
+
+      const spawnResult = await spawnInfoRefsAd({ repoPath, service });
+
+      if (!spawnResult.isOk) {
+        const error = spawnResult.error;
+        console.error(error);
+
+        if (error.code === "NO_ENTRY_ERROR") {
+          return c.text("Repository not found", 404);
+        }
+
+        return c.text("Internal server error", 500);
+      }
+
+      c.header("Content-Type", `application/x-${service}-advertisement`);
+      c.header("Cache-Control", "no-cache");
+      console.log(spawnResult.data);
+      return c.text(spawnResult.data, 200);
+    }
+  )
+  .post("/:repo/receive-pack", (c) => {
+    return c.text("OK", 200);
+  });
