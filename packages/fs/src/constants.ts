7f290bf00d65d8328b9284c39c3bb7e351d4f076
Ryan Luong <lumintriet@gmail.com>
2026-06-02 10:59:45 +0700

    feat(fs/constants): declare constants.

--- packages/fs/src/constants.ts
+++ packages/fs/src/constants.ts
@@ -1,2 +1,8 @@
-export const FS_NAME_MAX_LENGTH = 255;
-export const FS_PATH_MAX_LENGTH = 4096;
+export const FS_NAME_MAX_BYTE_LENGTH = 255;
+export const FS_PATH_MAX_BYTE_LENGTH = 4096;
+
+export const FS_NAME_ALLOWED_CHARACTERS_REGEX = /^[a-zA-Z0-9_.-]+$/;
+
+export const CLEAN_FILE_NAME_FORMAT = "clean_file_name";
+export const CLEAN_DIRECTORY_NAME_FORMAT = "clean_directory_name";
+export const CLEAN_PATH_FORMAT = "clean_path";
