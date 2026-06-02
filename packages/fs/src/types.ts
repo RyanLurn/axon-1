722647e13d9fd4ae74fcaf4e650deff4da26c8db
Ryan Luong <lumintriet@gmail.com>
2026-06-02 18:15:21 +0700

    refactor(fs/dead-code): remove unused code.

--- packages/fs/src/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Branded } from "@repo/types/branded";
-
-export type RealPath = Branded<string, "RealPath">;
-export type RealFilePath = Branded<string, "RealFilePath">;
-export type RealDirectoryPath = Branded<string, "RealDirectoryPath">;
-
-export type SafePath = Branded<string, "SafePath">;
-export type SafeFilePath = Branded<string, "SafeFilePath">;
-export type SafeDirectoryPath = Branded<string, "SafeDirectoryPath">;
