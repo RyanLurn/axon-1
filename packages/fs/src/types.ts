8da8f4f498b239123d54eafbd9488f14aaa27f52
Ryan Luong <lumintriet@gmail.com>
2026-06-02 07:53:21 +0700

    feat(fs/types): define SafePath branded type.

--- packages/fs/src/types.ts
+++ packages/fs/src/types.ts
@@ -2,6 +2,8 @@ import type { Branded } from "@repo/types/branded";
 
 export type RealPath = Branded<string, "RealPath">;
 
+export type SafePath = Branded<string, "SafePath">;
+
 export type FilePath = Branded<string, "FilePath">;
 
 export type DirectoryPath = Branded<string, "DirectoryPath">;
