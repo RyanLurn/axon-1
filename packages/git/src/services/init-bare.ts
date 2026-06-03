50cfaea2ff4df76ccaea5c447beeb2281ff2f2f6
Ryan Luong <lumintriet@gmail.com>
2026-06-03 09:50:47 +0700

    feat(git/init-bare): add a check before creating the bare repo.

--- packages/git/src/services/init-bare.ts
+++ packages/git/src/services/init-bare.ts
@@ -4,9 +4,49 @@ import { UnexpectedError } from "@repo/errors/unexpected";
 
 import type { RepoPath } from "@/utils/get-repo-path";
 
+import { RepoAlreadyInitializedError } from "@/errors/repo-already-initialized";
+
 export async function spawnInitBare(
   repoPath: RepoPath
-): Promise<Result<string, UnexpectedError>> {
+): Promise<Result<string, RepoAlreadyInitializedError | UnexpectedError>> {
+  try {
+    const revParseProcess = Bun.spawn(["git-rev-parse", "--git-dir"], {
+      cwd: repoPath,
+      stderr: "pipe",
+    });
+
+    const exitCode = await revParseProcess.exited;
+
+    if (exitCode === 0) {
+      return {
+        success: false,
+        error: new RepoAlreadyInitializedError({ repoPath }),
+      };
+    }
+
+    if (exitCode !== 128) {
+      const error = await revParseProcess.stderr.text();
+      return {
+        success: false,
+        error: new UnexpectedError({
+          message: `Failed to check if a repository has already been initialized in "${repoPath}".`,
+          cause: new Error(error),
+        }),
+      };
+    }
+
+    // If the exit code is 128, it's likely that a repo hasn't been initialized yet.
+    // In this case, we pass through to the next step of initializing a bare repo.
+  } catch (error) {
+    return {
+      success: false,
+      error: new UnexpectedError({
+        message: `Something went wrong while checking if a repository has already been initialized in "${repoPath}".`,
+        cause: error,
+      }),
+    };
+  }
+
   try {
     const gitProcess = Bun.spawn(["git-init", "--bare"], {
       cwd: repoPath,
