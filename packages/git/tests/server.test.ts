c56c5192055f0b5301f0fac556222eb85efcdf4e
Ryan Luong <lumintriet@gmail.com>
2026-06-03 10:02:40 +0700

    test(git/server): initialize a bare repo for testing during setup.

--- packages/git/tests/server.test.ts
+++ packages/git/tests/server.test.ts
@@ -1,11 +1,25 @@
 import { beforeAll, afterAll } from "bun:test";
 
+import type { RepoName } from "@/utils/validators";
+
+import { spawnInitBare } from "@/services/init-bare";
+import { getRepoPath } from "@/utils/get-repo-path";
 import { gitServer } from "@/index";
 
 let testServer: Bun.Server<undefined>;
+const testRepoName = "server-test-repo" as RepoName;
+const testRepoPath = getRepoPath(testRepoName);
 
-beforeAll(() => {
+beforeAll(async () => {
   console.log("[SETUP] Setting up Git CGI server test...");
+
+  const initTestRepoResult = await spawnInitBare(testRepoPath);
+  if (!initTestRepoResult.success) {
+    throw initTestRepoResult.error;
+  }
+  console.log(
+    `[SETUP] Initialized a bare repo for testing at ${testRepoPath}.`
+  );
 
   testServer = Bun.serve({
     port: 3000,
