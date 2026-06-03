0d965d5ce539dcf7fc9b00edcb5523e0ad83bbbd
Ryan Luong <lumintriet@gmail.com>
2026-06-03 07:20:41 +0700

    test(git/server): Add logging at the start of beforeAll hook.

--- packages/git/tests/server.test.ts
+++ packages/git/tests/server.test.ts
@@ -5,11 +5,13 @@ import { gitServer } from "@/index";
 let testServer: Bun.Server<undefined>;
 
 beforeAll(() => {
+  console.log("[SETUP] Setting up Git CGI server test...");
+
   testServer = Bun.serve({
     port: 3000,
     fetch: gitServer.fetch,
   });
-  console.log(`Test Git server running at ${testServer.url.href}`);
+  console.log(`[SETUP] Test Git server running at ${testServer.url.href}`);
 });
 
 afterAll(async () => {
