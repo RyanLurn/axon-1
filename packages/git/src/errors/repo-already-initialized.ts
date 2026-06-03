1f93fb7dd7f3d07fc40005450e62bdac70baf513
Ryan Luong <lumintriet@gmail.com>
2026-06-03 09:39:31 +0700

    feat(git/errors): define RepoAlreadyInitializedError class.

--- packages/git/src/errors/repo-already-initialized.ts
+++ packages/git/src/errors/repo-already-initialized.ts
@@ -2,17 +2,19 @@ import { BaseError } from "@repo/errors/base";
 
 import type { RepoPath } from "@/utils/get-repo-path";
 
-export class AlreadyABareRepoError extends BaseError<
-  "ALREADY_A_BARE_REPO_ERROR",
+export class RepoAlreadyInitializedError extends BaseError<
+  "REPO_ALREADY_INITIALIZED_ERROR",
   null
 > {
   repoPath: RepoPath;
 
   constructor({ message, repoPath }: { message?: string; repoPath: RepoPath }) {
     super({
-      name: "AlreadyABareRepoError",
-      message: message ?? `A bare repository already exists at "${repoPath}".`,
-      code: "ALREADY_A_BARE_REPO_ERROR",
+      name: "RepoAlreadyInitializedError",
+      message:
+        message ??
+        `A repository has already been initialized in "${repoPath}".`,
+      code: "REPO_ALREADY_INITIALIZED_ERROR",
       cause: null,
     });
     this.repoPath = repoPath;
