cc7a08bc3fd1d06680e3affae6a554f2cd3758cf
Ryan Luong <lumintriet@gmail.com>
2026-06-03 10:17:32 +0700

    test(fs/realpath): add error case for NoEntryError.

--- packages/fs/src/resolve-real-path.test.ts
+++ packages/fs/src/resolve-real-path.test.ts
@@ -1,10 +1,25 @@
 import { describe, expect, test } from "bun:test";
+import { join } from "node:path";
 
 import { resolveRealPath } from "@/resolve-real-path";
+import { NoEntryError } from "@/errors/no-entry";
 
 describe("resolveRealPath function should", () => {
   test("successfully resolve a path that exists on the file system", async () => {
     const resolveThisFileResult = await resolveRealPath(import.meta.path);
     expect(resolveThisFileResult.success).toBeTrue();
+  });
+
+  test("fail to resolve a path that doesn't exist on the file system", async () => {
+    expect.assertions(2);
+
+    const resolveFakeFileResult = await resolveRealPath(
+      join(import.meta.dir, "yo.wtf")
+    );
+    expect(resolveFakeFileResult.success).toBeFalse();
+
+    if (!resolveFakeFileResult.success) {
+      expect(resolveFakeFileResult.error).toBeInstanceOf(NoEntryError);
+    }
   });
 });
