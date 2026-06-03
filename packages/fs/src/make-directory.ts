d046ba30fdeb8a760ebced461a597e0b05c2e085
Ryan Luong <lumintriet@gmail.com>
2026-06-04 06:18:16 +0700

    feat(fs/mkdir): handle PathAlreadyExistsError in makeDirectory function.

--- packages/fs/src/make-directory.ts
+++ packages/fs/src/make-directory.ts
@@ -3,13 +3,17 @@ import type { Result } from "@repo/types/result";
 import { UnexpectedError } from "@repo/errors/unexpected";
 import { mkdir } from "node:fs/promises";
 
+import { PathAlreadyExistsError } from "@/errors/path-already-exists";
+
 export async function makeDirectory({
   path,
   recursive = false,
 }: {
   path: string;
   recursive?: boolean;
-}): Promise<Result<undefined | string, UnexpectedError>> {
+}): Promise<
+  Result<undefined | string, PathAlreadyExistsError | UnexpectedError>
+> {
   try {
     const mkdirResult = await mkdir(path, { recursive });
     return {
@@ -17,6 +21,20 @@ export async function makeDirectory({
       data: mkdirResult,
     };
   } catch (error) {
+    if (error instanceof Error && "errno" in error) {
+      const exception = error as ErrnoException;
+
+      if (exception.code === "EEXIST") {
+        return {
+          success: false,
+          error: new PathAlreadyExistsError({
+            message: `Failed to make a new directory at "${path}" because it already exists.`,
+            path,
+            cause: exception,
+          }),
+        };
+      }
+    }
     return {
       success: false,
       error: new UnexpectedError({
