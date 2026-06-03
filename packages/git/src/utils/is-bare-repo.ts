ca99f9bc66d67e4cd4e0cd2f4daee9f3bd7b6f64
Ryan Luong <lumintriet@gmail.com>
2026-06-03 08:37:12 +0700

    feat(git/utils): define isBareRepo util function.

---
+++ packages/git/src/utils/is-bare-repo.ts
@@ -0,0 +1,48 @@
+import type { Result } from "@repo/types/result";
+
+import { UnexpectedError } from "@repo/errors/unexpected";
+import { join } from "node:path";
+
+import type { RepoDir } from "@/utils/get-repo-dir";
+
+import { gitEnvVars } from "@/utils/env-vars";
+
+export async function isBareRepo(
+  repoDir: RepoDir
+): Promise<Result<boolean, UnexpectedError>> {
+  const repoPath = join(gitEnvVars.GIT_DIR_PATH, repoDir);
+
+  try {
+    const gitProcess = Bun.spawn(["git-rev-parse", "--is-bare-repository"], {
+      cwd: repoPath,
+      stderr: "pipe",
+    });
+
+    const exitCode = await gitProcess.exited;
+
+    if (exitCode !== 0) {
+      const error = await gitProcess.stderr.text();
+      return {
+        success: false,
+        error: new UnexpectedError({
+          message: `"git-rev-parse --is-bare-repository" child process exited with code "${exitCode}" after being spawned in "${repoPath}".`,
+          cause: new Error(error),
+        }),
+      };
+    }
+
+    const output = await gitProcess.stdout.text();
+    return {
+      success: true,
+      data: output.trim() === "true",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: new UnexpectedError({
+        message: `Something went wrong while spawning "git-rev-parse --is-bare-repository" child process in "${repoPath}".`,
+        cause: error,
+      }),
+    };
+  }
+}
