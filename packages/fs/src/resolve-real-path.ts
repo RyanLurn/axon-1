c271be3993a49132bfd959566cc1db1cfba96e46
Ryan Luong <lumintriet@gmail.com>
2026-06-04 07:47:57 +0700

    feat(fs/realpath): rewrite resolveRealPath function's error handling.

--- packages/fs/src/resolve-real-path.ts
+++ packages/fs/src/resolve-real-path.ts
@@ -3,11 +3,14 @@ import type { Result } from "@repo/types/result";
 import { UnexpectedError } from "@repo/errors/unexpected";
 import { realpath } from "node:fs/promises";
 
+import { PermissionDeniedError } from "@/errors/permission-denied";
 import { NoEntryError } from "@/errors/no-entry";
 
 export async function resolveRealPath(
   path: string
-): Promise<Result<string, UnexpectedError | NoEntryError>> {
+): Promise<
+  Result<string, PermissionDeniedError | UnexpectedError | NoEntryError>
+> {
   try {
     const resolvedPath = await realpath(path);
     return {
@@ -15,17 +18,29 @@ export async function resolveRealPath(
       data: resolvedPath,
     };
   } catch (error) {
-    if (error instanceof Error && "code" in error && error.code === "ENOENT") {
-      return {
-        success: false,
-        error: new NoEntryError({ path, cause: error as ErrnoException }),
-      };
+    if (error instanceof Error && "errno" in error) {
+      const cause = error as ErrnoException;
+
+      switch (cause.code) {
+        case "ENOENT": {
+          return {
+            success: false,
+            error: new NoEntryError({ path, cause }),
+          };
+        }
+        case "EACCES": {
+          return {
+            success: false,
+            error: new PermissionDeniedError({ path, cause }),
+          };
+        }
+      }
     }
 
     return {
       success: false,
       error: new UnexpectedError({
-        message: `An unexpected error occurred while resolving ${path}.`,
+        message: `An unexpected error occurred while resolving "${path}".`,
         cause: error,
       }),
     };
