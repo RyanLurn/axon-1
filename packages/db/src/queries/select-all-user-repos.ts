aa14a9bbf9daaea1ddd2cc8d9ab539cca060b73d
Ryan Luong <lumintriet@gmail.com>
2026-06-05 08:51:14 +0700

    feat(db/queries): define selectAllUserRepos function.

---
+++ packages/db/src/queries/select-all-user-repos.ts
@@ -0,0 +1,15 @@
+import { eq } from "drizzle-orm";
+
+import type { UserId } from "@/types/user-id";
+
+import { repoTable } from "@/schema/tables/git";
+import { db } from "@/index";
+
+export async function selectAllUserRepos(userId: UserId) {
+  const repos = await db
+    .select()
+    .from(repoTable)
+    .where(eq(repoTable.userId, userId));
+
+  return repos;
+}
