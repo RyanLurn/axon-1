2b765fff2c7c3468ac9a4e2e36ddab15bdd66a78
Ryan Luong <lumintriet@gmail.com>
2026-06-05 07:53:02 +0700

    feat(db/schema): declare agentTable.

---
+++ packages/db/src/schema/tables/agent.ts
@@ -0,0 +1,19 @@
+import { sqliteTable, index, text } from "drizzle-orm/sqlite-core";
+
+import type { AgentId } from "@/types/agent-id";
+
+import { timestamps } from "@/schema/helpers/timestamps";
+import { userId } from "@/schema/helpers/user-id";
+import { id } from "@/schema/helpers/id";
+
+export const agentTable = sqliteTable(
+  "agents",
+  {
+    id: id.$type<AgentId>(),
+    userId,
+    name: text("name").notNull(),
+    description: text("description"),
+    ...timestamps,
+  },
+  (table) => [index("agents_user_id_index").on(table.userId)]
+);
