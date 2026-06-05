b05c3a1d4737485c7b660167bceaa16b0f09f675
Ryan Luong <lumintriet@gmail.com>
2026-06-05 09:42:43 +0700

    feat(db/queries): define selectAllUserRepos's Result type.

--- packages/db/src/queries/select-all-user-repos.ts
+++ packages/db/src/queries/select-all-user-repos.ts
@@ -1,3 +1,6 @@
+import type { Result } from "@repo/types/result";
+
+import { UnexpectedError } from "@repo/errors/unexpected";
 import { eq } from "drizzle-orm";
 
 import type { SelectedUserRepo } from "@/types/inferred";
@@ -6,11 +9,26 @@ import type { UserId } from "@/types/branded";
 import { repoTable } from "@/schema/tables/git";
 import { db } from "@/index";
 
-export async function selectAllUserRepos(userId: UserId) {
-  const repos = await db
-    .select()
-    .from(repoTable)
-    .where(eq(repoTable.userId, userId));
+export async function selectAllUserRepos(
+  userId: UserId
+): Promise<Result<SelectedUserRepo[], UnexpectedError>> {
+  try {
+    const repos = await db
+      .select()
+      .from(repoTable)
+      .where(eq(repoTable.userId, userId));
 
-  return repos as SelectedUserRepo[];
+    return {
+      success: true,
+      data: repos as SelectedUserRepo[],
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: new UnexpectedError({
+        message: `An unexpected error occurred while selecting repos for user "${userId}".`,
+        cause: error,
+      }),
+    };
+  }
 }
