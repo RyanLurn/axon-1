0e074348d72902369e667aa0083d0de15e11f449
Ryan Luong <lumintriet@gmail.com>
2026-06-06 10:46:09 +0700

    feat(db/queries): deleteRepo now accepts RepoSelector to delete repo by name.

--- packages/db/src/queries/repo/delete.ts
+++ packages/db/src/queries/repo/delete.ts
@@ -3,29 +3,35 @@ import type { Result } from "@repo/types/result";
 import { UnexpectedError } from "@repo/errors/unexpected";
 import { and, eq } from "drizzle-orm";
 
-import type { RepoId, UserId } from "@/types/branded";
+import type { RepoSelector } from "@/types/selectors";
+import type { UserId } from "@/types/branded";
 
 import { repoTable } from "@/schema/tables/repo";
 import { db } from "@/index";
 
 export async function deleteRepo({
-  id,
+  selector,
   userId,
 }: {
-  id: RepoId;
+  selector: RepoSelector;
   userId: UserId;
 }): Promise<Result<null, UnexpectedError>> {
   try {
     await db
       .delete(repoTable)
-      .where(and(eq(repoTable.id, id), eq(repoTable.userId, userId)));
+      .where(
+        and(
+          eq(repoTable[selector.column], selector.value),
+          eq(repoTable.userId, userId)
+        )
+      );
 
     return { success: true, data: null };
   } catch (error) {
     return {
       success: false,
       error: new UnexpectedError({
-        message: `Something went wrong while deleting repo with id "${id}".`,
+        message: `Something went wrong while deleting repo with ${selector.column} "${selector.value}".`,
         cause: error,
       }),
     };
