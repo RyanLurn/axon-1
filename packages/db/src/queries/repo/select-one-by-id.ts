9ca8e9a589ae31a7278f0c91f1644dfb26483097
Ryan Luong <lumintriet@gmail.com>
2026-06-06 08:57:45 +0700

    feat(db/queries): define selectRepoById function.

---
+++ packages/db/src/queries/repo/select-one-by-id.ts
@@ -0,0 +1,21 @@
+import { and, eq } from "drizzle-orm";
+
+import type { RepoId, UserId } from "@/types/branded";
+
+import { repoTable } from "@/schema/tables/repo";
+import { db } from "@/index";
+
+export async function selectRepoById({
+  id,
+  userId,
+}: {
+  id: RepoId;
+  userId: UserId;
+}) {
+  const [selectedRepo] = await db
+    .select()
+    .from(repoTable)
+    .where(and(eq(repoTable.id, id), eq(repoTable.userId, userId)));
+
+  return selectedRepo ?? null;
+}
