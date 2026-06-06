ab3ed07c2af7d32b5babb6f3b9b9e4471fedb504
Ryan Luong <lumintriet@gmail.com>
2026-06-06 09:31:09 +0700

    feat(db/queries): define Result type for the selectRepo query function.

--- packages/db/src/queries/repo/select.ts
+++ packages/db/src/queries/repo/select.ts
@@ -1,6 +1,10 @@
+import type { Result } from "@repo/types/result";
+
+import { UnexpectedError } from "@repo/errors/unexpected";
 import { and, eq } from "drizzle-orm";
 
 import type { RepoId, UserId } from "@/types/branded";
+import type { SelectedRepo } from "@/types/inferred";
 
 import { repoTable } from "@/schema/tables/repo";
 import { db } from "@/index";
@@ -11,11 +15,21 @@ export async function selectRepo({
 }: {
   id: RepoId;
   userId: UserId;
-}) {
-  const [selectedRepo] = await db
-    .select()
-    .from(repoTable)
-    .where(and(eq(repoTable.id, id), eq(repoTable.userId, userId)));
+}): Promise<Result<SelectedRepo | null, UnexpectedError>> {
+  try {
+    const [selectedRepo] = await db
+      .select()
+      .from(repoTable)
+      .where(and(eq(repoTable.id, id), eq(repoTable.userId, userId)));
 
-  return selectedRepo ?? null;
+    return { success: true, data: selectedRepo ?? null };
+  } catch (error) {
+    return {
+      success: false,
+      error: new UnexpectedError({
+        message: `Something went wrong while selecting repo "${id}".`,
+        cause: error,
+      }),
+    };
+  }
 }
