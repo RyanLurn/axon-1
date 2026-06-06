2cbf6bf0c8c9ed7ff62fd3e51111cd785159a891
Ryan Luong <lumintriet@gmail.com>
2026-06-06 09:23:08 +0700

    feat(db/queries): define Result type for the insertRepo function.

--- packages/db/src/queries/repo/insert.ts
+++ packages/db/src/queries/repo/insert.ts
@@ -1,5 +1,11 @@
+import type { Result } from "@repo/types/result";
+
+import { UnexpectedError } from "@repo/errors/unexpected";
+import { SQLiteError } from "bun:sqlite";
+
 import type { RepoName, RepoId, UserId } from "@/types/branded";
 
+import { RepoNameTakenError } from "@/errors/repo-name-taken";
 import { repoTable } from "@/schema/tables/repo";
 import { db } from "@/index";
 
@@ -11,15 +17,35 @@ export async function insertRepo({
   userId: UserId;
   name: RepoName;
   description?: string;
-}): Promise<RepoId> {
-  const id = Bun.randomUUIDv7() as RepoId;
+}): Promise<Result<RepoId, RepoNameTakenError | UnexpectedError>> {
+  try {
+    const id = Bun.randomUUIDv7() as RepoId;
 
-  await db.insert(repoTable).values({
-    id,
-    userId,
-    name,
-    description,
-  });
+    await db.insert(repoTable).values({
+      id,
+      userId,
+      name,
+      description,
+    });
 
-  return id;
+    return { success: true, data: id };
+  } catch (error) {
+    if (
+      error instanceof SQLiteError &&
+      error.code === "SQLITE_CONSTRAINT_UNIQUE"
+    ) {
+      return {
+        success: false,
+        error: new RepoNameTakenError({ name, cause: error }),
+      };
+    }
+
+    return {
+      success: false,
+      error: new UnexpectedError({
+        message: `Something went wrong while inserting repo "${name}".`,
+        cause: error,
+      }),
+    };
+  }
 }
