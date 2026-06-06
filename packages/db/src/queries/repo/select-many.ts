7fe4a7aaf92b73bd5e3f1b70dbba7b6e6cd1e9f6
Ryan Luong <lumintriet@gmail.com>
2026-06-06 09:32:30 +0700

    feat(db/queries): define Result type for the selectRepos query function.

--- packages/db/src/queries/repo/select-many.ts
+++ packages/db/src/queries/repo/select-many.ts
@@ -1,10 +1,33 @@
+import type { Result } from "@repo/types/result";
+
+import { UnexpectedError } from "@repo/errors/unexpected";
 import { eq } from "drizzle-orm";
 
+import type { SelectedRepo } from "@/types/inferred";
 import type { UserId } from "@/types/branded";
 
 import { repoTable } from "@/schema/tables/repo";
 import { db } from "@/index";
 
-export async function selectManyRepos({ userId }: { userId: UserId }) {
-  return db.select().from(repoTable).where(eq(repoTable.userId, userId));
+export async function selectRepos({
+  userId,
+}: {
+  userId: UserId;
+}): Promise<Result<SelectedRepo[], UnexpectedError>> {
+  try {
+    const repos = await db
+      .select()
+      .from(repoTable)
+      .where(eq(repoTable.userId, userId));
+
+    return { success: true, data: repos };
+  } catch (error) {
+    return {
+      success: false,
+      error: new UnexpectedError({
+        message: "Something went wrong while selecting repos.",
+        cause: error,
+      }),
+    };
+  }
 }
