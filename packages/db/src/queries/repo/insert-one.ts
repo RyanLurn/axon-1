248e8d4498c6bf3da0c1d9391862d02690847d4a
Ryan Luong <lumintriet@gmail.com>
2026-06-06 08:48:57 +0700

    feat(db/queries): define insertRepo function.

---
+++ packages/db/src/queries/repo/insert-one.ts
@@ -0,0 +1,25 @@
+import type { RepoName, RepoId, UserId } from "@/types/branded";
+
+import { repoTable } from "@/schema/tables/repo";
+import { db } from "@/index";
+
+export async function insertRepo({
+  userId,
+  name,
+  description,
+}: {
+  userId: UserId;
+  name: RepoName;
+  description?: string;
+}): Promise<RepoId> {
+  const id = Bun.randomUUIDv7() as RepoId;
+
+  await db.insert(repoTable).values({
+    id,
+    userId,
+    name,
+    description,
+  });
+
+  return id;
+}
