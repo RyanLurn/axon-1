946a24d9aef5ebd04dc2e29b196159ebc3637798
Ryan Luong <lumintriet@gmail.com>
2026-06-07 17:55:19 +0700

    feat(user/storage): declare listReposFn server function.

--- apps/user/src/features/storage/server-functions/list-repos.ts
+++ apps/user/src/features/storage/server-functions/list-repos.ts
@@ -1,3 +1,20 @@
+import { setResponseStatus } from "@tanstack/react-start/server";
+import { selectRepos } from "@repo/db/queries/repo/select-many";
 import { createServerFn } from "@tanstack/react-start";
 
-export const listRepos = createServerFn().handler(async () => {});
+import { authMiddleware } from "@/features/auth/middleware";
+
+export const listReposFn = createServerFn()
+  .middleware([authMiddleware])
+  .handler(async ({ context }) => {
+    const selectReposResult = await selectRepos({
+      userId: context.user.id,
+    });
+
+    if (!selectReposResult.success) {
+      setResponseStatus(500);
+      throw new Error("Internal server error");
+    }
+
+    return selectReposResult.data;
+  });
