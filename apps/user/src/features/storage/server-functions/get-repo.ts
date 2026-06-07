4a35b9b350037f745c8210c2978c2962460bbb26
Ryan Luong <lumintriet@gmail.com>
2026-06-07 09:59:28 +0700

    feat(user/storage): declare getRepo server function.

---
+++ apps/user/src/features/storage/server-functions/get-repo.ts
@@ -0,0 +1,28 @@
+import { repoSelectorValidator } from "@repo/db/validators/repo/selector";
+import { setResponseStatus } from "@tanstack/react-start/server";
+import { selectRepo } from "@repo/db/queries/repo/select";
+import { createServerFn } from "@tanstack/react-start";
+import { notFound } from "@tanstack/react-router";
+
+import { authMiddleware } from "@/lib/auth/auth-middleware";
+
+export const getRepo = createServerFn()
+  .middleware([authMiddleware])
+  .inputValidator(repoSelectorValidator)
+  .handler(async ({ context, data }) => {
+    const selectRepoResult = await selectRepo({
+      selector: data,
+      userId: context.user.id,
+    });
+
+    if (!selectRepoResult.success) {
+      const error = selectRepoResult.error;
+      if (error.code === "REPO_NOT_FOUND_ERROR") {
+        throw notFound();
+      }
+      setResponseStatus(500);
+      throw new Error("Internal server error");
+    }
+
+    return selectRepoResult.data;
+  });
