f98bd7d7f805e256c791640231240372ee096cff
Ryan Luong <lumintriet@gmail.com>
2026-06-07 08:29:39 +0700

    feat(user/auth): improve error handling clarity.

--- apps/user/src/lib/auth/auth-middleware.ts
+++ apps/user/src/lib/auth/auth-middleware.ts
@@ -1,6 +1,7 @@
 import { FailedSessionUpdateError } from "@repo/auth/errors/failed-session-update";
 import { InternalAuthAPIError } from "@repo/auth/errors/internal-auth-api";
 import { setResponseStatus } from "@tanstack/react-start/server";
+import { UnexpectedError } from "@repo/errors/unexpected";
 import { createMiddleware } from "@tanstack/react-start";
 import { isAPIError } from "better-auth/api";
 import { userAuth } from "@repo/auth";
@@ -21,18 +22,18 @@ export const authMiddleware = createMiddleware().server(
       return next({ context: { user: session.user } });
     } catch (error) {
       if (isAPIError(error)) {
-        switch (error.status) {
-          case "INTERNAL_SERVER_ERROR": {
-            const authError = new InternalAuthAPIError(error);
-            console.error(authError);
-            break;
-          }
-          case "UNAUTHORIZED": {
-            const authError = new FailedSessionUpdateError(error);
-            console.error(authError);
-            break;
-          }
-        }
+        // From Better Auth's source code (see "https://github.com/better-auth/better-auth/blob/main/packages/better-auth/src/api/routes/session.ts#L461"), we know that if the status is "UNAUTHORIZED", it's because of the library "Handle case where session update fails (e.g., concurrent deletion)"
+        const authError =
+          error.status === "UNAUTHORIZED"
+            ? new FailedSessionUpdateError(error)
+            : new InternalAuthAPIError(error);
+        console.error(authError);
+      } else {
+        const unexpectedError = new UnexpectedError({
+          message: "Something went wrong while getting user's auth session.",
+          cause: error,
+        });
+        console.error(unexpectedError);
       }
 
       setResponseStatus(403);
