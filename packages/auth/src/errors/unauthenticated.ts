6bd5039bdf63ba140c3eb6e136c26c8895ccf7c3
Ryan Luong <lumintriet@gmail.com>
2026-06-07 17:03:16 +0700

    refactor(auth/errors): refactor UnauthenticatedError class.

--- packages/auth/src/errors/unauthenticated.ts
+++ packages/auth/src/errors/unauthenticated.ts
@@ -4,17 +4,17 @@ export class UnauthenticatedError extends BaseError<
   "UNAUTHENTICATED_ERROR",
   null
 > {
-  method: Request["method"];
-  url: Request["url"];
+  method: string;
+  url: string;
 
-  constructor(request: Request) {
+  constructor({ method, url }: { method: string; url: string }) {
     super({
       name: "UnauthenticatedError",
-      message: `An unauthenticated request made to "[${request.method}] ${request.url}".`,
+      message: `An unauthenticated request made to "[${method}] ${url}".`,
       code: "UNAUTHENTICATED_ERROR",
       cause: null,
     });
-    this.method = request.method;
-    this.url = request.url;
+    this.method = method;
+    this.url = url;
   }
 }
