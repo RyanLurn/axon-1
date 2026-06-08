322555b74f84a271109faf84474555fa3eb0c7a2
Ryan Luong <lumintriet@gmail.com>
2026-06-08 08:01:20 +0700

    feat(user/auth): set repsonse status to 401 in middleware.

--- apps/user/src/features/auth/middleware.ts
+++ apps/user/src/features/auth/middleware.ts
@@ -1,3 +1,4 @@
+import { setResponseStatus } from "@tanstack/react-start/server";
 import { getAppUser } from "@repo/auth/operations/get-app-user";
 import { createMiddleware } from "@tanstack/react-start";
 
@@ -7,6 +8,7 @@ export const authMiddleware = createMiddleware().server(
 
     if (!getAppUserResult.success) {
       console.error(getAppUserResult.error);
+      setResponseStatus(401);
       throw new Error("Unauthorized");
     }
 
