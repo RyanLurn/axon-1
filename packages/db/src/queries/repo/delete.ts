776632e30ff126314dc9117707fc121938856fea
Ryan Luong <lumintriet@gmail.com>
2026-06-08 07:46:12 +0700

    feat(db/queries): handle case where no repo was deleted.

--- packages/db/src/queries/repo/delete.ts
+++ packages/db/src/queries/repo/delete.ts
@@ -6,6 +6,7 @@ import { and, eq } from "drizzle-orm";
 import type { RepoSelector } from "@/validators/repo/selector";
 import type { UserId } from "@/types/branded";
 
+import { RepoNotFoundError } from "@/errors/repo-not-found";
 import { repoTable } from "@/schema/tables/repo";
 import { db } from "@/index";
 
@@ -15,16 +16,24 @@ export async function deleteRepo({
 }: {
   selector: RepoSelector;
   userId: UserId;
-}): Promise<Result<null, UnexpectedError>> {
+}): Promise<Result<null, RepoNotFoundError | UnexpectedError>> {
   try {
-    await db
+    const [deletedRepo] = await db
       .delete(repoTable)
       .where(
         and(
           eq(repoTable[selector.column], selector.value),
           eq(repoTable.userId, userId)
         )
-      );
+      )
+      .returning();
+
+    if (!deletedRepo) {
+      return {
+        success: false,
+        error: new RepoNotFoundError(selector),
+      };
+    }
 
     return { success: true, data: null };
   } catch (error) {
