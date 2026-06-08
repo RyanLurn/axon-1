fc7a8c3cc9c10868c2b22052a9d62cfd9c4693e0
Ryan Luong <lumintriet@gmail.com>
2026-06-08 18:46:50 +0700

    feat(user/nav): define ThemeToggle component.

---
+++ apps/user/src/components/nav/theme-toggle.tsx
@@ -0,0 +1,37 @@
+import { MonitorCog, Moon, Sun } from "lucide-react";
+
+import {
+  TooltipContent,
+  TooltipTrigger,
+  Tooltip,
+} from "@/components/ui/tooltip";
+import { useTheme } from "@/components/providers/theme";
+import { Button } from "@/components/ui/button";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  function cycleTheme() {
+    if (theme === "light") setTheme("dark");
+    else if (theme === "dark") setTheme("system");
+    else setTheme("light");
+  }
+
+  const ThemeIcon =
+    theme === "dark" ? Moon : theme === "light" ? Sun : MonitorCog;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger
+        render={
+          <Button onClick={cycleTheme} variant="ghost" size="icon-sm">
+            <ThemeIcon />
+          </Button>
+        }
+      />
+      <TooltipContent side="right">
+        {theme === "light" ? "Light" : theme === "dark" ? "Dark" : "System"}
+      </TooltipContent>
+    </Tooltip>
+  );
+}
