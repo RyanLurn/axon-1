5ee692439341de108a63c8da1dd5166e0b8b0453
Ryan Luong <lumintriet@gmail.com>
2026-06-10 20:58:21 +0700

    feat(user/storage): add UI elements to SettingsTab.

--- apps/user/src/routes/_authenticated/storage/$repoName/settings.tsx
+++ apps/user/src/routes/_authenticated/storage/$repoName/settings.tsx
@@ -1,11 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { DeleteRepoSection } from "@/features/storage/components/delete-repo-section";
+
 export const Route = createFileRoute(
-  "/_authenticated/storage/$repoName/settings",
+  "/_authenticated/storage/$repoName/settings"
 )({
-  component: RouteComponent,
+  component: SettingsTab,
 });
 
-function RouteComponent() {
-  return <div>Hello "/_authenticated/storage/$repoName/settings"!</div>;
+function SettingsTab() {
+  const { repoName } = Route.useParams();
+
+  return (
+    <div className="px-6 py-6">
+      <div className="max-w-md">
+        <DeleteRepoSection repoName={repoName} />
+      </div>
+    </div>
+  );
 }
