c8dceef0ff48c63d80f25c14d694fabd5ae5f901
Ryan Luong <lumintriet@gmail.com>
2026-06-10 16:59:12 +0700

    feat(user/auth): authMiddleware now throws UIError.

--- apps/user/src/features/auth/middleware.ts
+++ apps/user/src/features/auth/middleware.ts
@@ -2,6 +2,8 @@ import { setResponseStatus } from "@tanstack/react-start/server";
 import { getAppUser } from "@repo/auth/operations/get-app-user";
 import { createMiddleware } from "@tanstack/react-start";
 
+import { UIError } from "@/lib/error";
+
 export const authMiddleware = createMiddleware().server(
   async ({ next, request }) => {
     const getAppUserResult = await getAppUser(request);
@@ -9,7 +11,10 @@ export const authMiddleware = createMiddleware().server(
     if (!getAppUserResult.success) {
       console.error(getAppUserResult.error);
       setResponseStatus(401);
-      throw new Error("Unauthorized");
+      throw new UIError({
+        message: "Unauthenticated",
+        code: "UNAUTHENTICATED_ERROR",
+      });
     }
 
     return next({ context: { user: getAppUserResult.data } });
